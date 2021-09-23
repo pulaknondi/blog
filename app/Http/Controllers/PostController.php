a8f78f29721cdde3f6996a3785bd3ef7e685dd87
pulak nondi <pulaknc@gmail.com>
2021-09-23 09:00:03 +0600

    blog

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Session;
+use Exception;
+class PostController extends Controller
+{
+    public function StorePost(Request $request){
+      
+       
+    }
+}
